22d4620f0b1b2fa43cb423a9f5a179c09554cb9e
adoniagracio <40045910+adoniagracio@users.noreply.github.com>
2023-10-03 12:09:08 +0700

    Add files via upload

--- Main.js
+++ Main.js
@@ -1,3 +1,5 @@
+
+
 var typed = new Typed(".text",{
     strings:["Student at Binus University"],
     typeSpeed:100,
@@ -21,3 +23,59 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         }
     });
 });
+
+$(".step").click( function() {
+    $(this).addClass("active").prevAll().addClass("active");
+    $(this).nextAll().removeClass("active");
+});
+
+$(".step01").click( function() {
+    $("#line-progress").css("width", "3%");
+    $(".discovery").addClass("active").siblings().removeClass("active");
+});
+
+$(".step02").click( function() {
+    $("#line-progress").css("width", "25%");
+    $(".strategy").addClass("active").siblings().removeClass("active");
+});
+
+$(".step03").click( function() {
+    $("#line-progress").css("width", "50%");
+    $(".creative").addClass("active").siblings().removeClass("active");
+});
+
+$(".step04").click( function() {
+    $("#line-progress").css("width", "75%");
+    $(".production").addClass("active").siblings().removeClass("active");
+});
+
+$(".step05").click( function() {
+    $("#line-progress").css("width", "100%");
+    $(".analysis").addClass("active").siblings().removeClass("active");
+});
+
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const menu_items = document.querySelectorAll('nav .mainMenu li a');
+
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+// close menu when you click on a menu item 
+menu_items.forEach(item => {
+    item.addEventListener('click',function(){
+        close();
+    })
+})
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
